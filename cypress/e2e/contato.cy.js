1dc5991455b0d60d87d5958b61327440c5ebd678
Anissa <anissa.adv@gmail.com>
2026-02-04 09:13:15 -0300

    Automacao do contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,56 @@
+
+
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+     cy.visit('http://localhost:3000/index.html')
+  });
+
+
+  it('Deve preencher formulário de contato com sucesso', () => {
+   cy.get('[name="name"]').type('Anissa Ahmed')
+    cy.get('[name="email"]').type('anissa@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
+    cy.get('[name="name"]').clear()
+    cy.get('[name="email"]').type('anissa@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+    cy.get('[name="name"]').type('Anissa')
+    cy.get('[name="email"]').clear()
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem selecionar o assunto', () => {
+    cy.get('[name="name"]').type('Anissa')
+    cy.get('[name="email"]').type('anissa@teste.com')
+    cy.get('[name="message"]').type('mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto')
+
+
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher mensagem', () => {
+    cy.get('[name="name"]').type('Anissa')
+    cy.get('[name="email"]').type('anissa@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').clear()
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem')
+  })
+
+})
