b59e414ab75bb5efb5816a07be20843ae899a89a
Anissa <anissa.adv@gmail.com>
2026-02-09 14:48:13 -0300

    automacao catalogo busca

--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -4,23 +4,51 @@ import livro from "../fixtures/livros.json";
 
 describe('Funcionalidade: Busca no catalogo', () => {
 
-    beforeEach(() => {
-    cy.visit('catalog.html')
 
 });
 
 it('Deve fazer a busca do livro 1984 com sucesso', () => {
-cy.get('#search-input').type('1984')
-cy.get('.card > .card-body').should('contain', 1984)
+    cy.visit('http://localhost:3000/catalog.html')
+    cy.get('#search-input').type('1984')
+    cy.get('.card > .card-body').should('contain', 1984)
 
 
 });
 
 it('Deve fazer a busca do livro 1984 com massa de dados', () => {
-cy.get('#search-input').type(livro[1].livro)
-cy.get('.card > .card-body').should('contain', livro[1].livro)
-
-
+    cy.visit('http://localhost:3000/catalog.html')
+    cy.get('#search-input').type(livro[1].livro)
+    cy.get('.card > .card-body').should('contain', livro[1].livro)
 });
 
+it('Deve fazer a busca de um livro usando Fixture', () => {
+    cy.visit('http://localhost:3000/catalog.html')
+    cy.fixture('livros').then((cat) => {
+        cy.get('#search-input').type(cat[0].livro)
+        cy.get('.card > .card-body').should('contain', cat[0].livro)
+    })
 });
+
+
+it.only('Deve validar todos os livros da lista', () => {
+    cy.visit('http://localhost:3000/catalog.html')
+    cy.fixture('livros').then((cat) => {
+        cat.forEach(item => {
+            cy.get('#search-input').clear().type(item.livro)
+            cy.get('.card > .card-body').should('contain', item.livro)
+            
+
+
+
+        });
+
+
+    });
+
+
+
+})
+
+
+
+
